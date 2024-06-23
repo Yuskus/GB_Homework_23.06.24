4dd3003c4a9054515a8ab3c3e464d7384e4aebe3
Admin <Admin@DESKTOP-TMS7UKD>
2024-06-23 12:21:58 +0300

    Добавьте файлы проекта.

---
+++ Bits.cs
@@ -0,0 +1,57 @@
+﻿namespace HomeworkGB2
+{
+    public interface IBit
+    {
+        bool GetBitByPosition(byte index);
+        void SetBitByPosition(byte index, bool value);
+    }
+    internal class Bits : IBit
+    {
+        public long Value { get; private set; }
+        public int Size { get; private set; }
+        public Bits(byte value)
+        {
+            Value = value;
+            Size = sizeof(byte);
+        }
+        public Bits(int value)
+        {
+            Value = value;
+            Size = sizeof(int);
+        }
+        public Bits(long value)
+        {
+            Value = value;
+            Size = sizeof(long);
+        }
+        public bool this[byte index]
+        {
+            get => GetBitByPosition(index);
+            set => SetBitByPosition(index, value);
+        }
+        public bool GetBitByPosition(byte index)
+        {
+            return (Value & (1 << index)) != 0;
+        }
+        public void SetBitByPosition(byte index, bool value)
+        {
+            if (value)
+            {
+                Value |= (byte)(1 << index);
+            }
+            else
+            {
+                Value &= (byte)~(1 << index);
+            }
+        }
+
+        public static implicit operator long(Bits bits) => bits.Value;
+        public static explicit operator Bits(long value) => new(value);
+
+        public static implicit operator int(Bits bits) => (int)bits.Value;
+        public static explicit operator Bits(int value) => new(value);
+
+        public static implicit operator byte(Bits bits) => (byte)bits.Value;
+        public static explicit operator Bits(byte value) => new(value);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace HomeworkGB2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            byte myByte = 1;
+            int myInt = 2;
+            long myLong = 3;
+
+            Console.WriteLine("Переменная myByte типа байт после объявления: " + myByte);
+            Console.WriteLine("Переменная myInt типа инт после объявления: " + myInt);
+            Console.WriteLine("Переменная myLong типа лонг после объявления: " + myLong);
+
+            var bitByte = new Bits((byte)4);
+            var bitInt = new Bits((int)5);
+            var bitLong = new Bits((long)6);
+
+            Console.WriteLine("Класс Битс (байт) после объявления: " + bitByte.Value);
+            Console.WriteLine("Класс Битс (инт) после объявления: " + bitInt.Value);
+            Console.WriteLine("Класс Битс (лонг) после объявления: " + bitLong.Value);
+
+            myByte = bitByte;
+            myInt = bitInt;
+            myLong = bitLong;
+
+            Console.WriteLine("Переменная myByte типа байт после неявного присваивания класса Битс: " + myByte);
+            Console.WriteLine("Переменная myInt типа инт после неявного присваивания класса Битс: " + myInt);
+            Console.WriteLine("Переменная myLong типа лонг после неявного присваивания класса Битс: " + myLong);
+
+            byte newByte = 7;
+            int newInt = 8;
+            long newLong = 9;
+
+            Console.WriteLine("Новая переменная newByte типа байт после объявления: " + newByte);
+            Console.WriteLine("Новая переменная newInt типа инт после объявления: " + newInt);
+            Console.WriteLine("Новая переменная newLong типа лонг после объявления: " + newLong);
+
+            bitByte = (Bits)newByte;
+            bitInt = (Bits)newInt;
+            bitLong = (Bits)newLong;
+
+            Console.WriteLine("Класс Битс (байт) после явного присваивания переменной newByte типа байт: " + bitByte.Value);
+            Console.WriteLine("Класс Битс (инт) после явного присваивания переменной newInt типа инт: " + bitInt.Value);
+            Console.WriteLine("Класс Битс (лонг) после явного присваивания переменной newLong типа лонг: " + bitLong.Value);
+
+            Console.ReadKey(true);
+        }
+    }
+}
